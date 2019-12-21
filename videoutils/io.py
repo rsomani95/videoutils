de481a35b13811fa75c7f16fd1828dbe1410dd25
rsomani95 <rsomani95@gmail.com>
2019-12-21 20:59:49 +0530

    docstrings

--- videoutils/io.py
+++ videoutils/io.py
@@ -11,9 +11,26 @@ def read_video(fname         : Union[str, cv2.VideoCapture],
                end_idx       : Optional[int]=None,
                frame_stride  : Optional[int]=None,
                target_frames : Union[tuple, list, int, np.array]=None,
-               apply         : Callable=None) -> Union[torch.Tensor, list]:
-    """Flexible video reader where you can grab frames in different ways
+               apply         : Optional[Callable]=None) -> Union[torch.Tensor, list]:
+    """
+    Flexible video reader where you can grab frames in different ways
     and return as different dtypes.
+
+    **Args**
+
+    * `fname`        : a `cv2.VideoCapture` object, or path to the video file
+    * `start_idx`    : index of the frame where you'd like to start reading the file
+    * `end_idx`      : index of the frame where you'd like to end reading the file
+    * `frame_stride` : after reading the frame, every `frame_stride`th index will be read
+    * `target_frames`: if this arg is used, `start_idx`, `end_idx`, and `frame_stride` are
+                       ignored. Can be an
+            - `int`            : returns frame at this index
+            - `list`/`np.array`: returns frame at index of each element
+            - `tuple`          : a tuple like `(start_idx, end_idx, frame_stride)` where
+                                 `frame_stride`is optional. If this is what you want, using the
+                                 other args instead of `target_frames` is more flexible.
+    * `apply`         : a function that transforms a single `np.array`
+                        of shape `(height, width, channels)`
     """
     cap    = capture(fname)
     if target_frames is None: target_frames = get_target_frames(fname, start_idx, end_idx, frame_stride)
@@ -54,6 +71,7 @@ def capture(x: Union[str, cv2.VideoCapture]) -> cv2.VideoCapture:
 
 #Cell
 def get_target_frames(file, start, end, stride):
+    "flexibly get the indixes of the frames you want to grab"
     num_frames = capture(file).get(cv2.CAP_PROP_FRAME_COUNT)
     if start is not None:
         if end is None: return np.arange(0, num_frames, stride)
